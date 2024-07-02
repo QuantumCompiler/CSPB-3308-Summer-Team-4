96a447d7c2f4a089c75df35183338ed5ef03d783
jduran00 <jadu7619@colorado.edu>
2024-07-02 02:11:30 +0000

    database files

---
+++ recipe-genie/src/database/utils/supabase.ts
@@ -0,0 +1,11 @@
+// initalizing new Supabase client. This is the entrypoint to the rest if the superbase ecosystem
+
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = 'https://pxpnrvpahcyfucjfrcqq.supabase.co'
+
+const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4cG5ydnBhaGN5ZnVjamZyY3FxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk4NzYyNzksImV4cCI6MjAzNTQ1MjI3OX0.PAiSQNpqsrhJxDskatXkhLTsfsO-rmjdSci7epLdgd8
+
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
